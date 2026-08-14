8f5387e454530d94d631332abe80b6e4f8e20a73
Adrián González <adrian.g.job@gmail.com>
2026-08-14 18:36:24 +0200

    fix(backend): compose API routers explicitly

--- backend/routes.py
+++ backend/routes.py
@@ -1,2 +1,9 @@
-from backend.script_routes import router
-from backend import device_routes, auth_routes
+from fastapi import APIRouter
+from backend.script_routes import router as script_router
+from backend.device_routes import router as device_router
+from backend.auth_routes import router as auth_router
+
+router = APIRouter()
+router.include_router(script_router)
+router.include_router(device_router)
+router.include_router(auth_router)
